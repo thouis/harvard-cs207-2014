acc834bc17d38623867390d19bad962c49896ba4
Cris Cecka <ccecka@seas.harvard.edu>
2014-09-05 09:07:36 -0400

    HW0 Files

---
+++ Graph.hpp
@@ -0,0 +1,301 @@
+#ifndef CS207_GRAPH_HPP
+#define CS207_GRAPH_HPP
+
+/** @file Graph.hpp
+ * @brief An undirected graph type
+ */
+
+#include <algorithm>
+#include <vector>
+#include <cassert>
+
+#include "CS207/Util.hpp"
+#include "Point.hpp"
+
+
+/** @class Graph
+ * @brief A template for 3D undirected graphs.
+ *
+ * Users can add and retrieve nodes and edges. Edges are unique (there is at
+ * most one edge between any pair of distinct nodes).
+ */
+class Graph {
+ private:
+
+  // HW0: YOUR CODE HERE
+  // Use this space for declarations of important internal types you need
+  // later in the Graph's definition.
+  // (As with all the "YOUR CODE HERE" markings, you may not actually NEED
+  // code here. Just use the space if you need it.)
+
+ public:
+
+  /////////////////////////////
+  // PUBLIC TYPE DEFINITIONS //
+  /////////////////////////////
+
+  /** Type of this graph. */
+  typedef Graph graph_type;
+
+  /** Predeclaration of Node type. */
+  class Node;
+  /** Synonym for Node (following STL conventions). */
+  typedef Node node_type;
+
+  /** Predeclaration of Edge type. */
+  class Edge;
+  /** Synonym for Edge (following STL conventions). */
+  typedef Edge edge_type;
+
+  /** Type of indexes and sizes.
+      Return type of Graph::Node::index(), Graph::num_nodes(),
+      Graph::num_edges(), and argument type of Graph::node(size_type) */
+  typedef unsigned size_type;
+
+  ////////////////////////////////
+  // CONSTRUCTOR AND DESTRUCTOR //
+  ////////////////////////////////
+
+  /** Construct an empty graph. */
+  Graph() {
+    // HW0: YOUR CODE HERE
+  }
+  /** Default destructor */
+  ~Graph() = default;
+
+  /////////////
+  // General //
+  /////////////
+
+  /** Return the number of nodes in the graph.
+   *
+   * Complexity: O(1).
+   */
+  size_type size() const {
+    // HW0: YOUR CODE HERE
+    return 0;
+  }
+
+  /** Remove all nodes and edges from this graph.
+   * @post num_nodes() == 0 && num_edges() == 0
+   *
+   * Invalidates all outstanding Node and Edge objects.
+   */
+  void clear() {
+    // HW0: YOUR CODE HERE
+  }
+
+  /////////////////
+  // GRAPH NODES //
+  /////////////////
+
+  /** @class Graph::Node
+   * @brief Class representing the graph's nodes.
+   *
+   * Node objects are used to access information about the Graph's nodes.
+   */
+  class Node {
+   public:
+    /** Construct an invalid node.
+     *
+     * Valid nodes are obtained from the Graph class, but it
+     * is occasionally useful to declare an @i invalid node, and assign a
+     * valid node to it later. For example:
+     *
+     * @code
+     * Graph::node_type x;
+     * if (...should pick the first node...)
+     *   x = graph.node(0);
+     * else
+     *   x = some other node using a complicated calculation
+     * do_something(x);
+     * @endcode
+     */
+    Node() {
+      // HW0: YOUR CODE HERE
+    }
+
+    /** Return this node's position. */
+    const Point& position() const {
+      // HW0: YOUR CODE HERE
+      return Point();
+    }
+
+    /** Return this node's index, a number in the range [0, graph_size). */
+    size_type index() const {
+      // HW0: YOUR CODE HERE
+      return size_type(-1);
+    }
+
+    /** Test whether this node and @a x are equal.
+     *
+     * Equal nodes have the same graph and the same index.
+     */
+    bool operator==(const Node& x) const {
+      // HW0: YOUR CODE HERE
+      (void) x;          // Quiet compiler warning
+      return false;
+    }
+
+    /** Test whether this node is less than @a x in the global order.
+     *
+     * This ordering function is useful for STL containers such as
+     * std::map<>. It need not have any geometric meaning.
+     *
+     * The node ordering relation must obey trichotomy: For any two nodes x
+     * and y, exactly one of x == y, x < y, and y < x is true.
+     */
+    bool operator<(const Node& x) const {
+      // HW0: YOUR CODE HERE
+      (void) x;           // Quiet compiler warning
+      return false;
+    }
+
+   private:
+    // Allow Graph to access Node's private member data and functions.
+    friend class Graph;
+    // HW0: YOUR CODE HERE
+    // Use this space to declare private data members and methods for Node
+    // that will not be visible to users, but may be useful within Graph.
+    // i.e. Graph needs a way to construct valid Node objects
+  };
+
+  /** Synonym for size(). */
+  size_type num_nodes() const {
+    return size();
+  }
+
+  /** Add a node to the graph, returning the added node.
+   * @param[in] position The new node's position
+   * @post new size() == old size() + 1
+   * @post result_node.index() == old size()
+   *
+   * Complexity: O(1) amortized operations.
+   */
+  Node add_node(const Point& position) {
+    // HW0: YOUR CODE HERE
+    (void) position;      // Quiet compiler warning
+    return Node();        // Invalid node
+  }
+
+  /** Return the node with index @a i.
+   * @pre 0 <= @a i < num_nodes()
+   * @post result_node.index() == i
+   *
+   * Complexity: O(1).
+   */
+  Node node(size_type i) const {
+    // HW0: YOUR CODE HERE
+    (void) i;             // Quiet compiler warning
+    return Node();        // Invalid node
+  }
+
+  /////////////////
+  // GRAPH EDGES //
+  /////////////////
+
+  /** @class Graph::Edge
+   * @brief Class representing the graph's edges.
+   *
+   * Edges are order-insensitive pairs of nodes. Two Edges with the same nodes
+   * are considered equal if they connect the same nodes, in either order.
+   */
+  class Edge {
+   public:
+    /** Construct an invalid Edge. */
+    Edge() {
+      // HW0: YOUR CODE HERE
+    }
+
+    /** Return a node of this Edge */
+    Node node1() const {
+      // HW0: YOUR CODE HERE
+      return Node();      // Invalid Node
+    }
+
+    /** Return the other node of this Edge */
+    Node node2() const {
+      // HW0: YOUR CODE HERE
+      return Node();      // Invalid Node
+    }
+
+    /** Test whether this edge and @a x are equal.
+     *
+     * Equal edges are from the same graph and have the same nodes.
+     */
+    bool operator==(const Edge& x) const {
+      // HW0: YOUR CODE HERE
+      (void) x;          // Quiet compiler warning
+      return false;
+    }
+
+    /** Test whether this edge is less than @a x in the global order.
+     *
+     * This ordering function is useful for STL containers such as
+     * std::map<>. It need not have any geometric meaning.
+     *
+     * The edge ordering relation must obey trichotomy: For any two edges x
+     * and y, exactly one of x == y, x < y, and y < x is true.
+     */
+    bool operator<(const Edge& x) const {
+      // HW0: YOUR CODE HERE
+      (void) x;           // Quiet compiler warning
+      return false;
+    }
+
+   private:
+    // Allow Graph to access Edge's private member data and functions.
+    friend class Graph;
+    // HW0: YOUR CODE HERE
+    // Use this space to declare private data members and methods for Edge
+    // that will not be visible to users, but may be useful within Graph.
+    // i.e. Graph needs a way to construct valid Edge objects
+  };
+
+  /** Return the total number of edges in the graph.
+   *
+   * Complexity: No more than O(num_nodes() + num_edges()), hopefully less
+   */
+  size_type num_edges() const {
+    // HW0: YOUR CODE HERE
+    return 0;
+  }
+
+  /** Add an edge to the graph, or return the current edge if it already exists.
+   * @pre @a a and @a b are distinct valid nodes of this graph
+   * @return an Edge object e with e.node1() == @a a and e.node2() == @a b
+   * @post has_edge(@a a, @a b) == true
+   * @post If old has_edge(@a a, @a b), new num_edges() == old num_edges().
+   *       Else,                        new num_edges() == old num_edges() + 1.
+   *
+   * Can invalidate edge indexes -- in other words, old edge(@a i) might not
+   * equal new edge(@a i). Must not invalidate outstanding Edge objects.
+   *
+   * Complexity: No more than O(num_nodes() + num_edges()), hopefully less
+   */
+  Edge add_edge(const Node& a, const Node& b) {
+    // HW0: YOUR CODE HERE
+    (void) a, (void) b;   // Quiet compiler warning
+    return Edge();        // Invalid Edge
+  }
+
+  /** Return the edge with index @a i.
+   * @pre 0 <= @a i < num_edges()
+   *
+   * Complexity: No more than O(num_nodes() + num_edges()), hopefully less
+   */
+  Edge edge(size_type i) const {
+    // HW0: YOUR CODE HERE
+    (void) i;             // Quiet compiler warning
+    return Edge();        // Invalid Edge
+  }
+
+ private:
+
+  // HW0: YOUR CODE HERE
+  // Use this space for your Graph class's internals:
+  //   helper functions, data members, and so forth.
+
+};
+
+#endif
---
+++ primes.cpp
@@ -0,0 +1,50 @@
+#include "CS207/Util.hpp"
+
+/** Return true iff @a n is prime.
+ * @pre @a n >= 0
+ */
+bool is_prime(int n)
+{
+  assert(n >= 0);
+  for (int i = n-1; i > 0; --i)
+    if (n % i == 0)
+      return true;
+  return false;
+}
+
+int main()
+{
+  while (!std::cin.eof()) {
+    // How many primes to test? And should we print them?
+    std::cerr << "Input Number: ";
+    int n = 0;
+    CS207::getline_parsed(std::cin, n);
+    if (n <= 0)
+      break;
+
+    std::cerr << "Print Primes (y/n): ";
+    char confirm = 'n';
+    CS207::getline_parsed(std::cin, confirm);
+    bool print_primes = (confirm == 'y' || confirm == 'Y');
+
+    CS207::Clock timer;
+
+    // Loop and count primes from 2 up to n
+    int num_primes = 0;
+    for (int i = 2; i <= n; ++i) {
+      if (is_prime(i)) {
+        ++num_primes;
+        if (print_primes)
+          std::cout << i << std::endl;
+      }
+    }
+
+    double elapsed_time = timer.seconds();
+
+    std::cout << "There are " << num_primes
+              << " primes less than or equal to " << n << ".\n"
+              << "Found in " << (1000 * elapsed_time) << " milliseconds.\n\n";
+  }
+
+  return 0;
+}
